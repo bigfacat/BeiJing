cc9f457da7ed8cb2b43829247c1f315753d5aa56
cx <741207064@qq.com>
2019-03-12 13:56:03 +0800

    Signed-off-by: cx <741207064@qq.com>

--- JlueTaxSystemBeiJingBS/sbzs-cjpt-web/abacus/sbzs_res/commonDeliver.js
+++
@@ -1,5 +0,0 @@
-function goPageZf() {
-    url = "/sbzs-cjpt-web/nssb/sbzf/sbzf.do?gdslxDm=3&amp;cdId=24&amp;gnDm=gndm-24";
-    // window.location.href=url;
-    window.open(url,"_blank");
-}
--- JlueTaxSystemBeiJingBS/sbzs-cjpt-web/sbzs_res/lhfjssb/js/lhfjssb_cus.js
+++
@@ -1,9 +0,0 @@
-/**
- * 属新视图
- * Add by C.Q 20190131 23:35
- * @returns
- */
-function refreshView() {
-	viewEngine.formApply($('#viewCtrlId'));
-	viewEngine.tipsForVerify(document.body);
-}
